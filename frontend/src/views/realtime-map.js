a6061ad6f0ec48191ba1a50d48364c90833e6b6e
Marc Vila <marcvila10@hotmail.com>
2019-05-04 17:46:08 +0200

    Add realtime map

--- frontend/src/views/realtime-map.js
+++ frontend/src/views/realtime-map.js
@@ -1,21 +1,61 @@
 import React from 'react'
+import { Map, Marker, Popup, TileLayer as Basemap } from 'react-leaflet'
+
+import EntryAPI from '../api/entryAPI.js'
+import Entry from '../models/entry.js'
+
+const CARTO_BASEMAP = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
 
 export class RealTimeMapTab extends React.Component {
-  state = {
-    center: [41.4, 2.15],
-    zoom: 12.5
-  }
-
   constructor(props) {
     super(props)
+
+    this.state = {
+      entriesRows: [],
+      center: [41.4, 2.15],
+      zoom: 12.5
+    }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    EntryAPI.getAllEntries(false, response => {
+      var results = response.message.map(entry => {
+        return new Entry(
+          entry.id,
+          entry.license_plate,
+          entry.position.reverse(),
+          entry.range / 1000 // Meters to km
+        )
+      })
+      this.setState({ entriesRows: results })
+    })
+  }
 
   render() {
     const { center, zoom } = this.state
 
-    return <div />
+    return (
+      <div>
+        <Map center={center} zoom={zoom} style={mapStyles}>
+          <Basemap attribution="" url={CARTO_BASEMAP} />
+          {this.state.entriesRows.map(entry => (
+            <Marker key={`marker-${entry.id}`} position={entry.position}>
+              <Popup>
+                <div>
+                  ID: {entry.id}
+                  <br />
+                  License Plate: {entry.license_plate}
+                  <br />
+                  Position: {entry.position}
+                  <br />
+                  Range: {entry.range} km.
+                </div>
+              </Popup>
+            </Marker>
+          ))}
+        </Map>
+      </div>
+    )
   }
 }
 
