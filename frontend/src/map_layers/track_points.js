70e97760d73d87f75566ec03d54d3c57c7421d69
Marc Vila <marcvila10@hotmail.com>
2019-05-04 16:39:27 +0200

    Map working with historica data

--- frontend/src/map_layers/track_points.js
+++ frontend/src/map_layers/track_points.js
@@ -1,19 +1,31 @@
 export default {
   style: `
-#layer {
-marker-width: 45;
-marker-fill: white;
-marker-fill-opacity: 1;
-marker-file: url(https://cartodb-libs.global.ssl.fastly.net/cartodbui/assets/unversioned/images/alphamarker.png);
-marker-allow-overlap: true;
-marker-line-width: 1;
-marker-line-color: #FFFFFF;
-marker-line-opacity: 1;
-image-filters: colorize-alpha(#4b2991,#872ca2,#c0369d,#ea4f88,#fa7876,#f6a97a,#edd9a3);
-}
+  #layer {
+    marker-width: 7;
+    marker-fill: #EE4D5A;
+    marker-fill-opacity: 0.9;
+    marker-allow-overlap: true;
+    marker-line-width: 1;
+    marker-line-color: #FFFFFF;
+    marker-line-opacity: 1;
+  }
   `,
 
   source: `
-  SELECT * FROM "ecooltra-carto-04".hackaton_data_rentals
+  SELECT
+      cartodb_id, rental_id, start_point_lat, start_point_lon, end_point_lat, end_point_lon, started_at, ended_at, user_id, system_name, vehicle_external_id, duration_in_seconds, billable_duration_in_seconds, first_checkout_attempt_at, first_checkout_attempt_error, first_checkout_attempt_error_details, first_checkout_attempt_id, first_checkout_attempt_state, last_checkout_attempt_at, last_checkout_attempt_error, last_checkout_attempt_error_details, last_checkout_attempt_id, last_checkout_attempt_state, first_odometer_in_meters, first_odometer_recorded_at, last_odometer_in_meters, last_odometer_recorded_at, pause_duration, pause_duration_in_seconds, reservation_at, reservation_point,
+      ST_SetSRID(
+        ST_MakePoint(
+        start_point_lon,
+        start_point_lat
+      ), 4326) AS the_geom,
+      ST_Transform(
+        ST_Buffer(ST_SetSRID(
+        ST_MakePoint(
+        start_point_lon,
+        start_point_lat
+      ), 4326), 0.001)
+        , 3857) as the_geom_webmercator
+  FROM "ecooltra-carto-04".hackaton_data_rentals AS _camshaft_georeference_long_lat_analysis
   `
 }
--- frontend/src/views/map.js
+++ frontend/src/views/map.js
@@ -1,30 +1,16 @@
-// Import React and ReactDOM
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-
-// Import react-leaflet for the map / basemap components
+import React from 'react'
 import { Map, TileLayer as Basemap } from 'react-leaflet'
-
-// Import CARTO.js v4 <3
 import carto from 'carto.js'
 
-// Import our custom Layer component (it uses carto.js methods internally)
 import Layer from '../components/Layer'
-
-// Import the dataset we want to use
 import track_points from '../map_layers/track_points'
 
-// Some basic styles
-import '../App.css'
-
-// Voyager basemap <3
 const CARTO_BASEMAP = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
 
 export class MapTab extends React.Component {
-  // Add some initial state for the center and the zoom
   state = {
-    center: [41.4, 2.4],
-    zoom: 13
+    center: [41.4, 2.15],
+    zoom: 12.5
   }
 
   constructor(props) {
@@ -44,13 +30,9 @@ export class MapTab extends React.Component {
 
     return (
       <div>
-        {/* Create a Map component, with our center, zoom and some styles so it's centered in the browser */}
         <Map center={center} zoom={zoom} style={mapStyles}>
-          {/* Inside the Map component we add the basemap, in this case Voyager */}
           <Basemap attribution="" url={CARTO_BASEMAP} />
 
-          {/* Now can just add as many layers as we want with the Layer component */}
-          {/* which takes a source (sql or dataset name), styles (cartoCSS) and the client (our user/apiKey) */}
           <Layer
             source={track_points.source}
             style={track_points.style}
@@ -64,7 +46,7 @@ export class MapTab extends React.Component {
 
 const mapStyles = {
   width: '90vw',
-  height: '75vh',
+  height: '80vh',
   margin: '0 auto'
 }
 
