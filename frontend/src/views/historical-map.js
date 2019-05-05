21eed8f7d3fd7fe14b111855a6f920220416426d
Marc Vila <marcvila10@hotmail.com>
2019-05-05 11:59:42 +0200

    Change historical map

--- frontend/src/views/historical-map.js
+++ frontend/src/views/historical-map.js
@@ -30,22 +30,34 @@ export class HistoricalMapTab extends React.Component {
 
     return (
       <div>
-        <Map center={center} zoom={zoom} style={mapStyles}>
-          <Basemap attribution="" url={CARTO_BASEMAP} />
+        {false && (
+          <Map center={center} zoom={zoom} style={mapStyles}>
+            <Basemap attribution="" url={CARTO_BASEMAP} />
 
-          <Layer
-            source={track_points.source}
-            style={track_points.style}
-            client={this.cartoClient}
-          />
-        </Map>
+            <Layer
+              source={track_points.source}
+              style={track_points.style}
+              client={this.cartoClient}
+            />
+          </Map>
+        )}
+        <iframe
+          style={mapStyles}
+          frameborder="0"
+          src="https://cartoworkshops.carto.com/u/ecooltra-carto-04/builder/f0bcea7b-0959-4ecd-8dae-0062ff78bde2/embed"
+          allowfullscreen
+          webkitallowfullscreen
+          mozallowfullscreen
+          oallowfullscreen
+          msallowfullscreen
+        />
       </div>
     )
   }
 }
 
 const mapStyles = {
-  width: '90vw',
+  width: '95vw',
   height: '80vh',
   margin: '0 auto'
 }
